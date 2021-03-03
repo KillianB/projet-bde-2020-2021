c05a54426d70f72801aeceb6889eb59d2ac443e6
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-03 12:28:52 +0100

    Update README.md Ajout du pdf de l'énoncé du projet Ajout de ce que Sylouan a envoyé sur discord (schema + MySQL)

---
+++ db.sql
@@ -0,0 +1,80 @@
+CREATE TABLE `cheptels_par_commune`
+(
+ `id`                  int NOT NULL ,
+ `departement`         linestring NOT NULL ,
+ `commune`             linestring NOT NULL ,
+ `date`                year NOT NULL ,
+ `classement`          linestring NOT NULL ,
+ `total_bovins`        int NULL ,
+ `total_vaches`        int NULL ,
+ `vaches_laitieres`    int NULL ,
+ `vaches_allaitantes`  int NULL ,
+ `bovins_plus_un_an`   int NULL ,
+ `bovins_moins_un_an`  int NULL ,
+ `chevres`             int NULL ,
+ `brebis_nourrices`    int NULL ,
+ `brebis_laitieres`    int NULL ,
+ `total_porcins`       int NULL ,
+ `truies_reprod_50_kg` int NULL ,
+ `poulets`             int NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `communes_par_territoire`
+(
+ `id`          int NOT NULL ,
+ `commune`     linestring NOT NULL ,
+ `canton`      linestring NOT NULL ,
+ `departement` linestring NOT NULL ,
+ `territoire`  linestring NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `volumes_eau`
+(
+ `id`         int NOT NULL ,
+ `date`       year NOT NULL ,
+ `territoire` linestring NOT NULL ,
+ `volume`     bigint NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `faits`
+(
+ `id`      int NOT NULL ,
+ `commune` int NOT NULL ,
+ `volume`  int NOT NULL ,
+ `cheptel` int NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `fkIdx_62` (`commune`),
+CONSTRAINT `FK_61` FOREIGN KEY `fkIdx_62` (`commune`) REFERENCES `communes_par_territoire` (`id`),
+KEY `fkIdx_69` (`volume`),
+CONSTRAINT `FK_68` FOREIGN KEY `fkIdx_69` (`volume`) REFERENCES `volumes_eau` (`id`),
+KEY `fkIdx_76` (`cheptel`),
+CONSTRAINT `FK_75` FOREIGN KEY `fkIdx_76` (`cheptel`) REFERENCES `cheptels_par_commune` (`id`),
+ CONSTRAINT `check_41` CHECK ( communes_par_territoire.id )
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
