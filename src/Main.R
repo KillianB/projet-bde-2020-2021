e5ce0f8a36f9a714516ba9d9ae4ae2cd89e3d67e
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-10 17:16:31 +0100

    Mise à jour du script R (CANTON_LIB contient juste le nom du cantom et plus son digit type 2A01)

--- src/Main.R
+++ src/Main.R
@@ -7,6 +7,10 @@ is.Corse <- function(x) {
   return(!is.na(x) && nchar(x) > 1 && (substr(x, 1, 2) == "2A" || substr(x, 1, 2) == "2B"))
 }
 
+remove.digit.Corse <- function(x) {
+  return(substr(x, 8, nchar(x)))
+}
+
 ################################################################################
 #                            Importation des données                           #
 ################################################################################
@@ -21,13 +25,14 @@ FDS_RA_3010.2010 <- read.csv(file.choose(), header = T, dec = ".", sep = ";", na
 
 variable <- FDS_RA_3010.2000["CANTON_MOD"]
 variable.is.Corse <- apply(variable, 1, is.Corse)
-res <- variable$CANTON_MOD[variable.is.Corse]
+res <- FDS_RA_3010.2000["CANTON_LIB"]
+res <- apply(res, 1, remove.digit.Corse)
 
 FDS_RA_3010.2000.reduit <- data.frame(
   "ANNREF" = FDS_RA_3010.2000$ANNREF[variable.is.Corse],
   "CANTON" = FDS_RA_3010.2000$CANTON[variable.is.Corse],
   "CANTON_MOD" = FDS_RA_3010.2000$CANTON_MOD[variable.is.Corse],
-  "CANTON_LIB" = FDS_RA_3010.2000$CANTON_LIB[variable.is.Corse],
+  "CANTON_LIB" = res[variable.is.Corse],
   "RA_3010_DIM2" = FDS_RA_3010.2000$RA_3010_DIM2[variable.is.Corse],
   "RA_3010_DIM2_MOD" = FDS_RA_3010.2000$RA_3010_DIM2_MOD[variable.is.Corse],
   "RA_3010_DIM2_LIB" = FDS_RA_3010.2000$RA_3010_DIM2_LIB[variable.is.Corse],
@@ -60,13 +65,14 @@ write.csv(FDS_RA_3010.2000.reduit.omit, file="..\\csv_cheptel_2000_2010_corse\\F
 
 variable <- FDS_RA_3010.2010["CANTON_MOD"]
 variable.is.Corse <- apply(variable, 1, is.Corse)
-res <- variable$CANTON_MOD[variable.is.Corse]
+res <- FDS_RA_3010.2010["CANTON_LIB"]
+res <- apply(res, 1, remove.digit.Corse)
 
 FDS_RA_3010.2010.reduit <- data.frame(
   "ANNREF" = FDS_RA_3010.2010$ANNREF[variable.is.Corse],
   "CANTON" = FDS_RA_3010.2010$CANTON[variable.is.Corse],
   "CANTON_MOD" = FDS_RA_3010.2010$CANTON_MOD[variable.is.Corse],
-  "CANTON_LIB" = FDS_RA_3010.2010$CANTON_LIB[variable.is.Corse],
+  "CANTON_LIB" = res[variable.is.Corse],
   "RA_3010_DIM2" = FDS_RA_3010.2010$RA_3010_DIM2[variable.is.Corse],
   "RA_3010_DIM2_MOD" = FDS_RA_3010.2010$RA_3010_DIM2_MOD[variable.is.Corse],
   "RA_3010_DIM2_LIB" = FDS_RA_3010.2010$RA_3010_DIM2_LIB[variable.is.Corse],
