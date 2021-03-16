56cf4e22457ada16c63405a0383b4cdb1b881950
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-16 17:14:28 +0100

    Supression des csv trop lourd après correction Rectification de l'encodage des csv et du script R Passage en lowerCase des csv depuis le script R

--- src/Main.R
+++ src/Main.R
@@ -1,3 +1,8 @@
+################################################################################
+#                                 Bibliothèques                                #
+################################################################################
+
+library(splus2R)
 
 ################################################################################
 #                                   Fonctions                                  #
@@ -15,9 +20,10 @@ remove.digit.Corse <- function(x) {
 #                            Importation des données                           #
 ################################################################################
 
-# FDS_G_2141.2010 <- read.csv(file.choose(), header = T, dec = ",", sep = ";", na.strings = "............")
-FDS_RA_3010.2000 <- read.csv(file.choose(), header = T, dec = ".", sep = ";", na.strings = "")
-FDS_RA_3010.2010 <- read.csv(file.choose(), header = T, dec = ".", sep = ";", na.strings = "")
+FDS_RA_3010.2000 <- read.csv(file.choose(), header = T, dec = ".", sep = ";", na.strings = "", encoding = "UTF-8")
+FDS_RA_3010.2010 <- read.csv(file.choose(), header = T, dec = ".", sep = ";", na.strings = "", encoding = "UTF-8")
+communes_par_territoire_de_projet_de_la_collectivite_territoriale_de_corse <- read.csv(file.choose(), header = T, sep = ";", encoding = "UTF-8")
+volumeseaubrutefacturesparperimetres <- read.csv(file.choose(), header = T, sep = ";", encoding = "UTF-8")
 
 ################################################################################
 #                                Formatage 2000                                #
@@ -29,35 +35,35 @@ res <- FDS_RA_3010.2000["CANTON_LIB"]
 res <- apply(res, 1, remove.digit.Corse)
 
 FDS_RA_3010.2000.reduit <- data.frame(
+  "NOM" = lowerCase(FDS_RA_3010.2010$NOM[variable.is.Corse]),
   "ANNREF" = FDS_RA_3010.2000$ANNREF[variable.is.Corse],
-  "CANTON" = FDS_RA_3010.2000$CANTON[variable.is.Corse],
-  "CANTON_MOD" = FDS_RA_3010.2000$CANTON_MOD[variable.is.Corse],
-  "CANTON_LIB" = res[variable.is.Corse],
-  "RA_3010_DIM2" = FDS_RA_3010.2000$RA_3010_DIM2[variable.is.Corse],
+  "CANTON" = lowerCase(FDS_RA_3010.2000$CANTON[variable.is.Corse]),
+  "CANTON_MOD" = lowerCase(FDS_RA_3010.2000$CANTON_MOD[variable.is.Corse]),
+  "CANTON_LIB" = lowerCase(res[variable.is.Corse]),
+  "RA_3010_DIM2" = lowerCase(FDS_RA_3010.2000$RA_3010_DIM2[variable.is.Corse]),
   "RA_3010_DIM2_MOD" = FDS_RA_3010.2000$RA_3010_DIM2_MOD[variable.is.Corse],
-  "RA_3010_DIM2_LIB" = FDS_RA_3010.2000$RA_3010_DIM2_LIB[variable.is.Corse],
-  "RA_3010_DIM3" = FDS_RA_3010.2000$RA_3010_DIM3[variable.is.Corse],
+  "RA_3010_DIM2_LIB" = lowerCase(FDS_RA_3010.2000$RA_3010_DIM2_LIB[variable.is.Corse]),
+  "RA_3010_DIM3" = lowerCase(FDS_RA_3010.2000$RA_3010_DIM3[variable.is.Corse]),
   "RA_3010_DIM3_MOD" = FDS_RA_3010.2000$RA_3010_DIM3_MOD[variable.is.Corse],
-  "RA_3010_DIM3_LIB" = FDS_RA_3010.2000$RA_3010_DIM3_LIB[variable.is.Corse],
-  "N118" = FDS_RA_3010.2000$N118[variable.is.Corse],
+  "RA_3010_DIM3_LIB" = lowerCase(FDS_RA_3010.2000$RA_3010_DIM3_LIB[variable.is.Corse]),
+  "N118" = lowerCase(FDS_RA_3010.2000$N118[variable.is.Corse]),
   "N118_MOD" = FDS_RA_3010.2000$N118_MOD[variable.is.Corse],
-  "N118_LIB" = FDS_RA_3010.2000$N118_LIB[variable.is.Corse],
-  "N027" = FDS_RA_3010.2000$N027[variable.is.Corse],
+  "N118_LIB" = lowerCase(FDS_RA_3010.2000$N118_LIB[variable.is.Corse]),
+  "N027" = lowerCase(FDS_RA_3010.2000$N027[variable.is.Corse]),
   "N027_MOD" = FDS_RA_3010.2000$N027_MOD[variable.is.Corse],
-  "N027_LIB" = FDS_RA_3010.2000$N027_LIB[variable.is.Corse],
+  "N027_LIB" = lowerCase(FDS_RA_3010.2000$N027_LIB[variable.is.Corse]),
   "VALEUR" = FDS_RA_3010.2000$VALEUR[variable.is.Corse],
-  "QUALITE" = FDS_RA_3010.2000$QUALITE[variable.is.Corse],
-  "ï..NOM" = FDS_RA_3010.2000$ï..NOM[variable.is.Corse]
+  "QUALITE" = lowerCase(FDS_RA_3010.2000$QUALITE[variable.is.Corse])
 )
 
 summary(FDS_RA_3010.2000.reduit)
 
-write.csv(FDS_RA_3010.2000.reduit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2000_reduit.csv", row.names = FALSE)
+write.csv(FDS_RA_3010.2000.reduit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2000_reduit.csv", row.names = FALSE, fileEncoding = "UTF-8")
 
 FDS_RA_3010.2000.reduit.omit <- na.omit(FDS_RA_3010.2000.reduit)
 summary(FDS_RA_3010.2000.reduit.omit)
 
-write.csv(FDS_RA_3010.2000.reduit.omit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2000_reduit_omit.csv", row.names = FALSE)
+write.csv(FDS_RA_3010.2000.reduit.omit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2000_reduit_omit.csv", row.names = FALSE, fileEncoding = "UTF-8")
 
 ################################################################################
 #                                Formatage 2010                                #
@@ -69,32 +75,32 @@ res <- FDS_RA_3010.2010["CANTON_LIB"]
 res <- apply(res, 1, remove.digit.Corse)
 
 FDS_RA_3010.2010.reduit <- data.frame(
+  "NOM" = lowerCase(FDS_RA_3010.2010$NOM[variable.is.Corse]),
   "ANNREF" = FDS_RA_3010.2010$ANNREF[variable.is.Corse],
-  "CANTON" = FDS_RA_3010.2010$CANTON[variable.is.Corse],
-  "CANTON_MOD" = FDS_RA_3010.2010$CANTON_MOD[variable.is.Corse],
-  "CANTON_LIB" = res[variable.is.Corse],
-  "RA_3010_DIM2" = FDS_RA_3010.2010$RA_3010_DIM2[variable.is.Corse],
+  "CANTON" = lowerCase(FDS_RA_3010.2010$CANTON[variable.is.Corse]),
+  "CANTON_MOD" = lowerCase(FDS_RA_3010.2010$CANTON_MOD[variable.is.Corse]),
+  "CANTON_LIB" = lowerCase(res[variable.is.Corse]),
+  "RA_3010_DIM2" = lowerCase(FDS_RA_3010.2010$RA_3010_DIM2[variable.is.Corse]),
   "RA_3010_DIM2_MOD" = FDS_RA_3010.2010$RA_3010_DIM2_MOD[variable.is.Corse],
-  "RA_3010_DIM2_LIB" = FDS_RA_3010.2010$RA_3010_DIM2_LIB[variable.is.Corse],
-  "RA_3010_DIM3" = FDS_RA_3010.2010$RA_3010_DIM3[variable.is.Corse],
+  "RA_3010_DIM2_LIB" = lowerCase(FDS_RA_3010.2010$RA_3010_DIM2_LIB[variable.is.Corse]),
+  "RA_3010_DIM3" = lowerCase(FDS_RA_3010.2010$RA_3010_DIM3[variable.is.Corse]),
   "RA_3010_DIM3_MOD" = FDS_RA_3010.2010$RA_3010_DIM3_MOD[variable.is.Corse],
-  "RA_3010_DIM3_LIB" = FDS_RA_3010.2010$RA_3010_DIM3_LIB[variable.is.Corse],
-  "N118" = FDS_RA_3010.2010$N118[variable.is.Corse],
+  "RA_3010_DIM3_LIB" = lowerCase(FDS_RA_3010.2010$RA_3010_DIM3_LIB[variable.is.Corse]),
+  "N118" = lowerCase(FDS_RA_3010.2010$N118[variable.is.Corse]),
   "N118_MOD" = FDS_RA_3010.2010$N118_MOD[variable.is.Corse],
-  "N118_LIB" = FDS_RA_3010.2010$N118_LIB[variable.is.Corse],
-  "N027" = FDS_RA_3010.2010$N027[variable.is.Corse],
+  "N118_LIB" = lowerCase(FDS_RA_3010.2010$N118_LIB[variable.is.Corse]),
+  "N027" = lowerCase(FDS_RA_3010.2010$N027[variable.is.Corse]),
   "N027_MOD" = FDS_RA_3010.2010$N027_MOD[variable.is.Corse],
-  "N027_LIB" = FDS_RA_3010.2010$N027_LIB[variable.is.Corse],
+  "N027_LIB" = lowerCase(FDS_RA_3010.2010$N027_LIB[variable.is.Corse]),
   "VALEUR" = FDS_RA_3010.2010$VALEUR[variable.is.Corse],
-  "QUALITE" = FDS_RA_3010.2010$QUALITE[variable.is.Corse],
-  "ï..NOM" = FDS_RA_3010.2010$ï..NOM[variable.is.Corse]
+  "QUALITE" = lowerCase(FDS_RA_3010.2010$QUALITE[variable.is.Corse])
 )
 
 summary(FDS_RA_3010.2010.reduit)
 
-write.csv(FDS_RA_3010.2010.reduit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2010_reduit.csv", row.names = FALSE)
+write.csv(FDS_RA_3010.2010.reduit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2010_reduit.csv", row.names = FALSE, fileEncoding = "UTF-8")
 
 FDS_RA_3010.2010.reduit.omit <- na.omit(FDS_RA_3010.2010.reduit)
 summary(FDS_RA_3010.2010.reduit.omit)
 
-write.csv(FDS_RA_3010.2010.reduit.omit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2010_reduit_omit.csv", row.names = FALSE)
+write.csv(FDS_RA_3010.2010.reduit.omit, file="..\\csv_cheptel_2000_2010_corse\\FDS_RA_3010_2010_reduit_omit.csv", row.names = FALSE, fileEncoding = "UTF-8")
