aa59e3a8a4ba26b00e8869dfc0e88140779e38c5
KillianB <killianbarreau@gmail.com>
2021-03-23 17:18:41 +0100

    Modif script création db

--- src/db.sql
+++ src/db.sql
@@ -15,23 +15,23 @@ DROP TABLE faits CASCADE CONSTRAINTS;
 CREATE TABLE cheptels_par_canton
 (
     id               number(10) NOT NULL,
-    nom              varchar(45) NOT NULL,
+    nom              varchar(100) NOT NULL,
     annref           number(4) NOT NULL,
-    canton           varchar(45) NOT NULL,
-    canton_mod       varchar(45) NOT NULL,
-    canton_lib       varchar(45) NULL,
-    ra_3010_dim2     varchar(45) NULL,
+    canton           varchar(100) NOT NULL,
+    canton_mod       varchar(100) NOT NULL,
+    canton_lib       varchar(100) NULL,
+    ra_3010_dim2     varchar(100) NULL,
     ra_3010_dim2_mod number(10) NULL,
-    ra_3010_dim2_lib varchar(45) NULL,
-    ra_3010_dim3     varchar(45) NULL,
+    ra_3010_dim2_lib varchar(100) NULL,
+    ra_3010_dim3     varchar(100) NULL,
     ra_3010_dim3_mod number(10) NULL,
-    ra_3010_dim3_lib varchar(45) NULL,
-    n118             varchar(45) NULL,
+    ra_3010_dim3_lib varchar(100) NULL,
+    n118             varchar(100) NULL,
     n118_mod         float(10) NULL,
-    n118_lib         varchar(45) NULL,
-    n027             varchar(45) NULL,
+    n118_lib         varchar(100) NULL,
+    n027             varchar(100) NULL,
     n027_mod         float(10) NULL,
-    n027_lib         varchar(45) NULL,
+    n027_lib         varchar(100) NULL,
     valeur           number(10) NULL,
     qualite          varchar(3) NULL,
 
@@ -47,10 +47,10 @@ CREATE TABLE cheptels_par_canton
 CREATE TABLE communes_par_territoire
 (
     id          number(10) NOT NULL,
-    commune     varchar(45) NOT NULL,
-    canton      varchar(45) NOT NULL,
-    departement varchar(45) NOT NULL,
-    territoire  varchar(45) NOT NULL,
+    commune     varchar(100) NOT NULL,
+    canton      varchar(100) NOT NULL,
+    departement varchar(100) NOT NULL,
+    territoire  varchar(100) NOT NULL,
 
     CONSTRAINT communes_par_territoire_pk PRIMARY KEY (id)
 );
@@ -65,7 +65,7 @@ CREATE TABLE communes_par_territoire
 CREATE TABLE territoires
 (
     id           number(10) NOT NULL,
-    territoire_1 varchar(45) NOT NULL,
+    territoire_1 varchar(100) NOT NULL,
 
     CONSTRAINT territoires_pk PRIMARY KEY (id)
 );
@@ -79,7 +79,7 @@ CREATE TABLE territoires
 CREATE TABLE corresp_canton_territoire
 (
     id     number(10) NOT NULL,
-    canton varchar(45) NOT NULL,
+    canton varchar(100) NOT NULL,
     id_territoire     number(10) NOT NULL,
 
     CONSTRAINT corresp_canton_territoire_pk PRIMARY KEY (id),
