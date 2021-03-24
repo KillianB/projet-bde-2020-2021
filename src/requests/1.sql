b56330897c5865e4ea2760b4447f7949bb2c3f71
KillianB <killianbarreau@gmail.com>
2021-03-24 14:38:52 +0100

    Modif req

--- src/requests/1.sql
+++ src/requests/1.sql
@@ -2,7 +2,7 @@
     Consommation d’eau par canton, département, territoire
  */
 
-select c.canton, c.departement, c.territoire, f.volume
+select c.canton, c.departement, c.territoire, sum(f.volume)
 from communes_par_territoire c, faits f, volumes_eau v, corresp_canton_territoire ct
 where f.id_volume = v.id and v.id_territoire = ct.id_territoire and ct.canton = c.canton
 group by rollup(c.canton, c.departement, c.territoire)
--- src/requests/5.sql
+++ src/requests/5.sql
@@ -4,7 +4,7 @@
 
 select c.territoire, c.departement, sum(f.valeur) as nbr
 from communes_par_territoire c, cheptels_par_canton ch, faits f
-where f.id_commune = c.id and f.id_canton = c.id and c.canton = ch.canton and
+where f.id_commune = c.id and f.id_cheptel = ch.id and c.canton = ch.canton and
         f.ra_3010_dim2_mod <> 4 and
         f.ra_3010_dim2_mod <> 5 and
         f.ra_3010_dim2_mod <> 6 and
