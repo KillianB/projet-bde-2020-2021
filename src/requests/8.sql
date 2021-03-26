5993bf76ae65e4d013f53d38c651e8e09d185614
keason7 <thattallpanda@gmail.com>
2021-03-26 17:26:50 +0100

    sql request fixed

--- src/requests/8.sql
+++ src/requests/8.sql
@@ -1,6 +1,6 @@
 /*  Consommation d’eau par taille des exploitations et cheptels */
 
-SELECT V.volume AS ConsoEau, C.ra_3010_dim3_mod AS tailleCheptel, C.n118_mod AS tailleExploitation
-FROM admi_49.volumes_eau V, admi_49.cheptels_par_canton C, admi_49.faits F
-WHERE C.id = F.id_cheptel, V.id = F.id_volume
-GROUP BY ROLLUP(V.volume, C.ra_3010_dim3_mod, C.n118_mod);
+SELECT SUM(F.volume), C.ra_3010_dim3_mod, C.n118_mod
+FROM admi_49.volumes_eau V, admi_49.cheptels_par_canton C, admi_49.faits F, admi_49.corresp_canton_territoire CT
+WHERE V.id = F.id_volume AND V.id_territoire = CT.id_territoire AND CT.canton = C.canton
+GROUP BY ROLLUP(C.ra_3010_dim3_mod, C.n118_mod);
