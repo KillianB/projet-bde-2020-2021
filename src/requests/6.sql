a08e501ff740f696a6f55c5d99e8bd50af8abf6f
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-26 19:01:37 +0100

    Fix request 6

--- src/requests/6.sql
+++ src/requests/6.sql
@@ -7,4 +7,7 @@ SELECT  canton_lib,
         SUM(valeur) AS nb_animaux,
         CAST(SUM(valeur)/(SELECT SUM(valeur) FROM admi_49.cheptels_par_canton)*100 AS integer) AS pourcentage_animaux
 FROM    admi_49.cheptels_par_canton cheptels_par_canton
+WHERE   ra_3010_dim2_mod <> 4 AND
+        ra_3010_dim2_mod <> 5 AND
+        ra_3010_dim2_mod <> 6
 GROUP BY CUBE(canton_lib, ra_3010_dim2_lib)
