e5b52f9d5586b0056d35115e14545498d9e8cdb4
KillianB <killianbarreau@gmail.com>
2021-03-26 16:58:12 +0100

    Modif req

--- src/requests/1.sql
+++ src/requests/1.sql
@@ -5,4 +5,4 @@
 select c.canton, c.departement, c.territoire, sum(f.volume)
 from communes_par_territoire c, faits f, volumes_eau v, corresp_canton_territoire ct
 where f.id_volume = v.id and v.id_territoire = ct.id_territoire and ct.canton = c.canton
-group by rollup(c.canton, c.departement, c.territoire)
+group by rollup(c.canton, c.departement), c.territoire
