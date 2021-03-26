71bf7dbf19ede0572073ddb010f5bfc3f0136cfd
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-26 18:25:48 +0100

    Fix en partie la requête 2

--- src/requests/2.sql
+++ src/requests/2.sql
@@ -2,29 +2,34 @@
     Consommation d’eau par cheptels et nombre d’animaux
  */
  
-SELECT canton, id_cheptel, SUM(faits.valeur) AS total_animaux, ((SUM(faits.valeur)/AC.total_animaux_canton)*EC.volume_eau) AS volume_eau
-FROM faits, cheptels_par_canton,
-    (SELECT canton, SUM(faits.valeur) AS total_animaux_canton
-    FROM faits, cheptels_par_canton
-    WHERE   faits.id = cheptels_par_canton.id_cheptel AND
-            ra_3010_dim2_mod <> 4 AND
-            ra_3010_dim2_mod <> 5 AND
-            ra_3010_dim2_mod <> 6
-    GROUP BY canton) AS AC,
-    (SELECT communes_par_territoire.canton AS canton, SUM(faits.volume) AS volume_eau
-    FROM faits, communes_par_territoire, volumes_eau, corresp_canton_territoire
-    WHERE   faits.id_commune = communes_par_territoire.id AND
-            faits.id_volume = volumes_eau.id AND
-            volumes_eau.id_territoire = corresp_canton_territoire.id_territoire AND
-            communes_par_territoire.canton = corresp_canton_territoire.canton AND
-            volumes_eau.date IN (SELECT DISTINCT annref
-                                FROM cheptels_par_canton)
-    GROUP BY communes_par_territoire.canton) AS EC
-WHERE   faits.id = cheptels_par_canton.id_cheptel AND
-        AC.canton = cheptels_par_canton.canton AND
-        AC.canton = EC.canton AND
-        ra_3010_dim2_mod <> 4 AND
-        ra_3010_dim2_mod <> 5 AND
-        ra_3010_dim2_mod <> 6 AND
-        cheptels_par_canton.annref = volume_eau.date
-GROUP BY ROLLUP(canton, id_cheptel)
+SELECT EC.canton, id_cheptel, SUM(faits.valeur) AS total_animaux, (SUM(faits.valeur)/AC.total_animaux_canton)*EC.volume_eau AS volume_eau
+FROM admi_49.faits faits,
+     admi_49.cheptels_par_canton cheptels_par_canton,
+     admi_49.volumes_eau volumes_eau,
+    (SELECT corresp.canton, SUM(volumes.volume) AS volume_eau
+      FROM admi_49.faits faits,
+           admi_49.communes_par_territoire communes, 
+           admi_49.volumes_eau volumes,
+           admi_49.corresp_canton_territoire corresp
+      WHERE   faits.id_commune = communes.id AND
+              faits.id_volume = volumes.id AND
+              volumes.id_territoire = corresp.id_territoire AND
+              communes.canton = corresp.canton AND
+              volumes.annee IN (SELECT annref
+                                FROM admi_49.cheptels_par_canton)
+      GROUP BY corresp.canton) EC,
+    (SELECT canton_lib, SUM(valeur) AS total_animaux_canton
+      FROM admi_49.cheptels_par_canton
+      WHERE   ra_3010_dim2_mod <> 4 AND
+              ra_3010_dim2_mod <> 5 AND
+              ra_3010_dim2_mod <> 6
+      GROUP BY canton_lib) AC
+WHERE   faits.id_cheptel = cheptels_par_canton.id AND
+        faits.id_volume = volumes_eau.id AND
+        AC.canton_lib = cheptels_par_canton.canton_lib AND
+        AC.canton_lib = EC.canton AND
+        faits.ra_3010_dim2_mod <> 4 AND
+        faits.ra_3010_dim2_mod <> 5 AND
+        faits.ra_3010_dim2_mod <> 6 AND
+        cheptels_par_canton.annref = admi_49.volumes_eau.annee
+GROUP BY ROLLUP(EC.canton, faits.id_cheptel)
