2c53ed991b6ca3aadc4d8b153de0dd06e1f2b57d
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-26 18:02:51 +0100

    Fix requête 6

--- src/requests/6.sql
+++ src/requests/6.sql
@@ -2,8 +2,9 @@
     Pour chaque canton, ratio moyen de chacun des types d’animaux
  */
 
-SELECT canton, ra_3010_dim2_mod, ra_3010_dim2_lib, SUM(valeur) AS nb_animaux, SUM(valeur)/total_cheptels AS ratio_animaux
-FROM    cheptels_par_canton,
-        (SELECT COUNT(*) AS total_cheptels
-        FROM cheptels_par_canton)
-GROUP BY CUBE(canton, ra_3010_dim2_mod)
+SELECT  canton_lib,
+        ra_3010_dim2_lib,
+        SUM(valeur) AS nb_animaux,
+        CAST(SUM(valeur)/(SELECT COUNT(*) AS total_cheptels FROM cheptels_par_canton)*100 AS integer) AS pourcentage_animaux
+FROM    admi_49.cheptels_par_canton cheptels_par_canton
+GROUP BY CUBE(canton_lib, ra_3010_dim2_lib)
