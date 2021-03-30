71254c8ff23039f559d51ec4dc877dfcb82fb7e8
Damien MARTIN <lescrimeur.damien@hotmail.fr>
2021-03-30 17:18:31 +0200

    Refactor script Python

--- src/script.py
+++ src/script.py
@@ -1,8 +1,7 @@
 import csv
 
-
-
-with open('territoires.csv') as csv_file:
+#Recuperation des noms de territoires et de leurs id
+with open('../CSVFINAL/territoires.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
     cpt = 0
@@ -11,33 +10,44 @@ with open('territoires.csv') as csv_file:
 
     for row in csv_reader:
         arrayid.append(cpt)
-        arrayterri.append(row[0])
+        arrayterri.append(row[1])
         cpt +=1
 
+    #Suppression des nom de colonnes (ligne 0)
     arrayid.pop(0)
     arrayterri.pop(0)
-"""
-    for i in range(len(arrayid)):
-        print(arrayid[i])
-        print(arrayterri[i])
-"""
 
-with open('volumes_eau_brute_factures_par_perimetres.csv') as csv_file:
+#Ouverture du fichier CSV a modifier
+with open('../csv_volEau_&_communes/volumes_eau_brute_factures_par_perimetres.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+
+    #Indexs correspondant aux noms de territoires
     index = []
+    #Rows du nouveau csv
+    new_rows = []
 
+    #On parcourt toutes les rows du fichier a modifier
     for row in csv_reader:
-        if line_count != 0:
+        if line_count != 0:     #Si la row n'est pas un nom de colonne
 
+            #Recuperation de l'index correspondant au nom de territoire de la row
             for i in range(0,len(arrayterri)):
-                if(row[1] == arrayterri[i]):
-                    #print(row[1])
-                    #print("ID : "+ str(arrayid[i]))
+                if(row[2] == arrayterri[i]):
                     index.append(arrayid[i])
 
-            #print(f'\t{row[1]} | {row[2]}')
+            #Ajout de la row contenant un id_terrioire au lieu d'un nom_territoire
+            size = len(index)
+            new_rows.append([str(row[0]),str(row[1]),str(index[size-1]),str(row[3])])
         line_count += 1
 
-for i in range(0,len(index)):
-    print(index[i])
+
+    #Affichage des nouvelles rows
+    #for i in range(0,len(new_rows)):
+        #print(new_rows[i])
+
+#Construction du nouveau fichier
+with open('../CSVFINAL/volumes_eau_brute_factures_par_perimetres.csv','w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerows([['id', 'date', 'territoire', 'volume_eau_brute']])
+    csv_writer.writerows(new_rows)
