6422b0dd0d3b974e147c5818d184726fc75f42b7
Sylouan CORFA <E157038T@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2021-03-30 15:31:13 +0200

    Fix final request 3

--- src/requests/3.sql
+++ src/requests/3.sql
@@ -1,8 +1,5 @@
--- Consommation d'eau par année et nombre d'animaux
-SELECT  c.annref,
-  (SUM(v.volume)/(SUM(c.valeur))) AS eau_par_animal
-FROM admi_49.faits f, admi_49.volumes_eau v, admi_49.cheptels_par_canton c
-WHERE f.id_volume = v.id 
-    AND f.id_cheptel = c.id
-    AND c.annref = v.annee
-GROUP BY ROLLUP(c.annref);
+-- Consommation d'eau par animal et par an
+SELECT annref,((SUM(f.volume))/(SUM(f.valeur))) as test
+FROM admi_49.faits f, ADMI_49.cheptels_par_canton c
+WHERE f.id_cheptel = c.id
+GROUP BY annref;
