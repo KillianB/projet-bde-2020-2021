6261b8a0f73e9401c8b0e23eeea13fa9744b0dc8
Sylouan CORFA <E157038T@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2021-03-30 14:25:27 +0200

    Fix requests 3 et 7

--- src/requests/3.sql
+++ src/requests/3.sql
@@ -1,7 +1,8 @@
 -- Consommation d'eau par année et nombre d'animaux
-SELECT f.volume, v.date, SUM(faits.valeur) as total_animaux
-FROM faits f, volumes_eau v, cheptels_par_canton c
+SELECT  c.annref,
+  (SUM(v.volume)/(SUM(c.valeur))) AS eau_par_animal
+FROM admi_49.faits f, admi_49.volumes_eau v, admi_49.cheptels_par_canton c
 WHERE f.id_volume = v.id 
     AND f.id_cheptel = c.id
-    AND c.annref = v.date
-GROUP BY ROLLUP(v.daten, total_animaux);
+    AND c.annref = v.annee
+GROUP BY ROLLUP(c.annref);
--- src/requests/7.sql
+++ src/requests/7.sql
@@ -1,6 +1,4 @@
 -- Nombre d'animaux par taille des exploitations et cheptels
-
-SELECT f.valeur AS animaux, c.ra_3010_dim3_mod AS tailleCheptel, c.n118_mod AS tailleExploitation
-FROM faits f, cheptels_par_canton c 
-WHERE c.id = f.id_cheptel
-GROUP BY ROLLUP(tailleCheptel, tailleExploitation)
+SELECT SUM(c.valeur), C.ra_3010_dim3_mod, C.n118_mod
+FROM admi_49.cheptels_par_canton C
+GROUP BY ROLLUP(C.ra_3010_dim3_mod, C.n118_mod);
