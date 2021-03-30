f50dd3564ebcbc2bad3d53af1ee1ccb71757bbfb
Sylouan CORFA <sylouan.corfa@etu.univ-nantes.fr>
2021-03-30 15:37:14 +0200

    Typo

--- src/requests/3.sql
+++ src/requests/3.sql
@@ -1,5 +1,5 @@
 -- Consommation d'eau par animal et par an
-SELECT annref,((SUM(f.volume))/(SUM(f.valeur))) as test
+SELECT annref,((SUM(f.volume))/(SUM(f.valeur))) as "eau par animal"
 FROM admi_49.faits f, ADMI_49.cheptels_par_canton c
 WHERE f.id_cheptel = c.id
 GROUP BY annref;
